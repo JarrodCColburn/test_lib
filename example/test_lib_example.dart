e034532eb3f5e0851b4285f2270fefe0997ee719
Jarrod Colburn <jcourtlandcolburn@gmail.com>
2018-12-28 19:09:03 +0000

    used stagehand to create simple package

---
+++ example/test_lib_example.dart
@@ -0,0 +1,6 @@
+import 'package:test_lib/test_lib.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/test_lib.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library test_lib;
+
+export 'src/test_lib_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
